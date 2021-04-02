612382de5d799b1a294e243db278d0cd27d52de3
klee8880 <klee8880@gmail.com>
2021-04-01 23:07:13 -0500

    Display test BRC data

--- src/main/java/leek/byteShark/view/BRCController.java
+++ src/main/java/leek/byteShark/view/BRCController.java
@@ -1,16 +1,33 @@
 package leek.byteShark.view;
 
+import java.io.IOException;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import leek.byteShark.controller.CommandManager;
+import leek.byteShark.model.CRBDataModel.CRBBase;
+import leek.byteShark.model.CRBDataModel.CRBData;
+
 @Controller
 public class BRCController {
 
+	private static String ADDRESS= "./InvoiceTestData.txt";
+	
 	@GetMapping("/brc")
-	public String showAll() {
+	public String showBRC(Model model){
+		
 		
 		//TODO: Populate the table
+		try {
+			List<CRBBase> brc = CommandManager.importNewBRC(ADDRESS);
+			List<CRBData> generalLines = CommandManager.extractDataLines(brc);
+			
+			model.addAttribute("BRCLines",generalLines);
+			
+		} catch (IOException e) {e.printStackTrace();}
 		
 		return "BRCTable";
 	}
