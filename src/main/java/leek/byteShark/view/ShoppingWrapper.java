a61294d1d35fc417aaf96f229a95e2203b1d71c5
Kevin Lee <klee8880@gmail.com>
2021-04-12 23:43:52 -0500

    Added input fields for global data

--- src/main/java/leek/byteShark/view/ShoppingWrapper.java
+++ src/main/java/leek/byteShark/view/ShoppingWrapper.java
@@ -8,29 +8,27 @@ import lombok.Data;
 
 public class ShoppingWrapper {
 
-	private String billedParty;
-	private LocalDate accountDate;
-	private String invoiceNumber;
-	private char priceMaster;
+//	private String billedParty;
+//	private LocalDate accountDate;
+//	private String invoiceNumber;
+//	private char priceMaster;
 	
-	private LocalDate invoiceDate;
+//	private LocalDate invoiceDate;
 	private String taxPayerId;
-	private int paymentTerms;
-	private LocalDate paymentDueDate;
 	
-	private String detailSource;
-	private String carInitial;
-	private int carNumber;
-	private char carType;
-	private char loadIndicator;
-	private LocalDate repairDate;
-	private int SPLC;
+//	private String detailSource;
+//	private String carInitial;
+//	private int carNumber;
+//	private char carType;
+//	private char loadIndicator;
+//	private LocalDate repairDate;
+//	private int SPLC;
 	
-	private String defectParty;
-	private LocalDate defectDate;
+//	private String defectParty;
+//	private LocalDate defectDate;
 	private char resubmittedInvoice;
 	private String originalInvoiceNumber;
 	private LocalDate originalAccountDate;
-	private LocalDate arrivalDate;
+//	private LocalDate arrivalDate;
 	
 }
